6285358483c81089803c1d9c656998e732deb007
Isaac Shapira <fresheyeball@gmail.com>
2015-04-22 10:02:32 -0700

    how to use lift?

--- src/TransformerFun.hs
+++ src/TransformerFun.hs
@@ -2,6 +2,7 @@ module TransformerFun where
 
 import Control.Applicative
 import Control.Monad
+import Control.Monad.Trans
 import Control.Monad.Trans.Maybe
 import Test.QuickCheck
 
@@ -11,6 +12,12 @@ g = MaybeT [Just 3, Nothing, Just 3]
 f, h :: Integer -> Integer -> Maybe Integer
 f x y = liftM (+ x) (Just y)
 h x y = (+ x) <$> (Just y)
+
+fobots = return 3 :: IO Integer
+
+-- hmmm, how to use lift?
+--i :: Integer -> Integer -> MaybeT IO Integer
+--i x y = return $ (x +) <$> fobots
 
 glukkon :: IO ()
 glukkon = do
