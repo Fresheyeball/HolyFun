63b27cfbebff3b1212d39b9b032a86b5a1e21e46
Isaac Shapira <fresheyeball@gmail.com>
2015-04-27 13:50:28 -0700

    more plus

--- src/Main.hs
+++ src/Main.hs
@@ -1,6 +1,6 @@
 module Main where
 
-import TransformerFun
+import MonadPlusFun
 
 main :: IO ()
 main = glukkon
--- src/MonadPlusFun.hs
+++ src/MonadPlusFun.hs
@@ -1,1 +1,28 @@
 module MonadPlusFun where
+
+import Control.Monad
+import Control.Applicative
+import Data.Monoid
+import Test.QuickCheck
+
+-- in the specific case, MonadPlus is the same as Monoid
+h, g :: [Int] -> [Int] -> [Int]
+h = mplus
+g = (<>)
+
+j :: Show a => [a] -> [a] -> [String]
+j x y = show <$> y `mplus` x
+
+-- mplus of Maybe just picks the one on the left, unless its Nothing
+k, s :: Maybe Int
+k = Just 3 `mplus` Just 6
+s = Nothing `mplus` Just 3
+
+-- Either is not a Monoid?
+--q :: Either String Int
+--q = Left "foo" `mplus` Right 3
+
+glukkon :: IO ()
+glukkon = do
+    print $ show k <> " | " <> show s
+    quickCheck $ \x y -> h x y == g x y
