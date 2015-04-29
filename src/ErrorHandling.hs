36b74a751ad2109bafa652557fab7bf3996711a5
Isaac Shapira <fresheyeball@gmail.com>
2015-04-28 20:36:13 -0700

    smaller with ify

--- src/ErrorHandling.hs
+++ src/ErrorHandling.hs
@@ -3,6 +3,7 @@ module ErrorHandling where
 import Data.Monoid
 import Control.Monad.Trans
 import Control.Monad.Trans.Error
+import If
 
 data Fubar = Fubar String
 
@@ -13,9 +14,6 @@ instance Error Fubar where
 fobots :: IO Int
 fobots = return 4
 
-if' :: Bool -> t -> t -> t
-if' c t e = if c then t else e
-
 dangerous :: ErrorT Fubar IO Int
-dangerous = do x <- lift fobots
-               if' (x < 5) (return x) (throwError . Fubar $ "less than 5!")
+dangerous = lift fobots >>=
+    ify (< 5) return (const . throwError . Fubar $ "less than 5!")
