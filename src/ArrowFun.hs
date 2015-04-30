46267c8184c76dbfd33f9a75e6ee354c3b40f6ac
Isaac Shapira <fresheyeball@gmail.com>
2015-04-29 19:11:45 -0700

    box drawing sucks

--- src/ArrowFun.hs
+++ src/ArrowFun.hs
@@ -9,6 +9,9 @@ murf = (***)
 buff :: (Int, Int) -> (Int, Int)
 buff = (+ 3) *** (+ 4)
 
+--               (y, z)
+-- x  ┳→ (+ 3) ┛  │
+--    ┖→ (+ 4) ───┙
 chuff :: Integer -> (Integer, Integer)
 chuff = (+ 3) &&& (+ 4)
 
