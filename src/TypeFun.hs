08c79f624940afaa1da3c74f7c50a0920ee89be6
Isaac Shapira <fresheyeball@gmail.com>
2015-04-30 12:30:30 -0700

    wtf types

--- src/TypeFun.hs
+++ src/TypeFun.hs
@@ -6,3 +6,16 @@ type family Foo a
 -- what in the unholy fuck?
 -- pretty sure this is evil
 type instance Foo Int = String
+type instance Foo String = Int
+
+
+-- not ok?
+{-
+foozle :: a -> Foo a
+foozle _ = "what"
+foozle _ = 3
+-}
+
+-- psycho, just psycho
+foozle :: Int -> Foo Int
+foozle _ = "what"
